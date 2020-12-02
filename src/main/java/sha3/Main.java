7e12db34191f8900da5a4ebbd24ef47383875e52
Sammy <13516103@std.stei.itb.ac.id>
2020-12-02 00:29:37 +0000

    Add another demo message

--- src/main/java/sha3/Main.java
+++ src/main/java/sha3/Main.java
@@ -1,5 +1,7 @@
 package sha3;
 
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
         byte[] m;
@@ -9,23 +11,13 @@ public class Main {
         sha3[2] = new Sha3(384);
         sha3[3] = new Sha3(512);
         m = new byte[0];
-        System.out.print("Message: ");
-        printHex(m);
-        System.out.println();
-        printDigest(sha3[0], m);
-        printDigest(sha3[1], m);
-        printDigest(sha3[2], m);
-        printDigest(sha3[3], m);
+        printDigests(sha3, m);
         m = new byte[200];
         for (int i = 0; i < m.length; ++i)
             m[i] = (byte)0xa3;
-        System.out.print("Message: ");
-        printHex(m);
-        System.out.println();
-        printDigest(sha3[0], m);
-        printDigest(sha3[1], m);
-        printDigest(sha3[2], m);
-        printDigest(sha3[3], m);
+        printDigests(sha3, m);
+        m = "abc".getBytes(StandardCharsets.US_ASCII);
+        printDigests(sha3, m);
     }
 
     private static void printHex(byte[] bb) {
@@ -38,6 +30,15 @@ public class Main {
         byte[] digest = md.digest(message);
         System.out.print(String.format(" SHA3-%d = ", s));
         printHex(digest);
+        System.out.println();
+    }
+
+    private static void printDigests(Sha3[] mds, byte[] m) {
+        System.out.print("Message: ");
+        printHex(m);
+        System.out.println("\n");
+        for (Sha3 md : mds)
+            printDigest(md, m);
         System.out.println();
     }
 }
